787ce64b9c01a19677baac1925464fbceb9b5381
Fay Vor <phavorsparks@gmail.com>
2018-08-09 12:09:45 +0100

    Add app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NdecisionApp from "./components/NdecisionApp";
+import 'normalize.css/normalize.css'
+import './styles/style.scss'
+
+
+ReactDOM.render(<NdecisionApp />, document.getElementById("app"));
