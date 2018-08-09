7c05659e5acbf9c90f913e4a27d16c57ef5854b3
Fay Vor <phavorsparks@gmail.com>
2018-08-09 12:08:35 +0100

    Add components

---
+++ src/components/AddOption.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class AddOption extends React.Component {
+  state = {
+    error: undefined
+  }
+
+  handleAddOption = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.addOption.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.addOption.value = "";
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption} className="add-option">
+          <input className="add-option__input" type="text" name="addOption" placeholder="Add an option" />
+          <button className="button">Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
